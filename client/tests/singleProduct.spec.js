caec3b8ccadf6d93475b5149eee0e770d1865eef
Rick Terry <fdterr@gmail.com>
2018-07-25 15:29:49 -0400

    Singe Product tests

---
+++ client/tests/singleProduct.spec.js
@@ -0,0 +1,33 @@
+/* global describe beforeEach it */
+
+import { expect } from 'chai';
+import React from 'react';
+import enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { UserHome, SingleProduct } from '../components';
+
+const product = {
+  imageUrl: 'www.placeholder.com',
+  name: 'Product Test',
+  price: 9.99,
+  description: 'Placeholder description goes here',
+};
+
+const adapter = new Adapter();
+enzyme.configure({ adapter });
+
+describe('UserHome', () => {
+  let singleProduct;
+
+  beforeEach(() => {
+    singleProduct = shallow(<SingleProduct product={product} />);
+  });
+
+  it("renders a product equal with an image, name, price, and description'", () => {
+    // console.log(singleProduct.find('.product-image'));
+    expect(singleProduct.find('.product-image').length).to.be.equal(1);
+    expect(singleProduct.find('.product-name').length).to.be.equal(1);
+    expect(singleProduct.find('.product-price').length).to.be.equal(1);
+    expect(singleProduct.find('.product-description').length).to.be.equal(1);
+  });
+});
