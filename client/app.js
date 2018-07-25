b9f3968e972416e0ec337c8ef5c24042689bcec8
Rick Terry <fdterr@gmail.com>
2018-07-25 13:22:23 -0400

    troubleshooting

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import HomePage from './HomePage'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <HomePage />
     </div>
   )
 }
