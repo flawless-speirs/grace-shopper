e36582859407adc474b9f0fec9f6b089ddedf221
Rick Terry <fdterr@gmail.com>
2018-07-25 13:29:12 -0400

    different message

--- client/components/Creator.js
+++ client/components/Creator.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-export default Creator = (props) => {
-  const name = props.user.name
-  const title = props.user.title
-  const description = props.user.description
-  const imageUrl = props.user.imageUrl
+export default (Creator = props => {
+  const name = props.user.name;
+  const title = props.user.title;
+  const description = props.user.description;
+  const imageUrl = props.user.imageUrl;
   const urlArray = props.user.urlArray;
 
   return (
-    <div className='profile'>
-      <div className='headshot'><img src={imageUrl} /></div>
-      <div className='profileRight'>
-        <div className='profileName'>{name}</div>
-        <div className='profileTitle'>{title}</div>
-        <div className='profileDescription'>{description}</div>
-        <div className='profileLinks'>{urlArray.map(url => {
-          return(<div className='profileLink'><img src={url.image} /></div>)
-        })}
+    <div>
+      <div className="profile">
+        <div className="headshot">
+          <img src={imageUrl} />
         </div>
+        <div className="profileRight">
+          <div className="profileName">{name}</div>
+          <div className="profileTitle">{title}</div>
+          <div className="profileDescription">{description}</div>
+          <div className="profileLinks">
+            {urlArray.map(url => {
+              return (
+                <div className="profileLink">
+                  <img src={url.image} />
+                </div>
+              );
+            })}
+          </div>
         </div>
-        </div>
-  )
-}
+      </div>
+    </div>
+  );
+});
