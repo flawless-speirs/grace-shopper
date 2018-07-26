41053e9b0925c05d3935ca987a77cce37244a074
Andrew Miller <admiller9@gmail.com>
2018-07-26 19:25:37 -0400

    cart component displays products in cart, buttons dispatch  add/remove actions

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,14 +3,41 @@ import { connect } from 'react-redux';
 import ProductRow from './ProductRow';
 import CheckoutForm from './CheckoutForm';
 import { Elements } from 'react-stripe-elements';
+import { products as getProducts } from '../store/products';
+import { getFromDB } from '../store/cart';
 
 /**
  * COMPONENT
  */
 
 class Cart extends Component {
+  constructor() {
+    super();
+    this.state = { products: [] };
+  }
+
+  async componentDidMount() {
+    await this.props.retrieveProducts();
+    await this.props.getCart();
+    let products = [];
+    if (this.props.cart) {
+      this.props.cart.forEach((value, key) => {
+        const toAdd = this.props.products.find(product => product.id === key);
+        if (toAdd) {
+          products.push({
+            name: toAdd.name,
+            price: toAdd.price,
+            quantity: value,
+          });
+        }
+      });
+    }
+    this.setState({ products });
+  }
+
   render() {
-    return (
+    const products = this.state.products;
+    return products.length ? (
       <div>
         <div className="container-fluid text-center">
           <div className="row text-center">
@@ -19,13 +46,22 @@ class Cart extends Component {
             <div className="col-2"> Quantity </div>
             <div className="col-2"> Amount </div>
           </div>
-          <ProductRow />
+          {products.map(product => {
+            console.log(product);
+            return (
+              <div key={product.id}>
+                <ProductRow product={product} />
+              </div>
+            );
+          })}
           <div> Total </div>
         </div>
         <Elements>
           <CheckoutForm />
         </Elements>
       </div>
+    ) : (
+      <div>Loading...</div>
     );
   }
 }
@@ -34,8 +70,14 @@ class Cart extends Component {
  * CONTAINER
  */
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  cart: state.cart,
+  products: state.products,
+});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  retrieveProducts: () => dispatch(getProducts()),
+  getCart: () => dispatch(getFromDB()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- client/components/ProductRow.js
+++ client/components/ProductRow.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { addToCart, removeFromCart } from '../store/cart';
 
 /**
  * COMPONENT
  */
 
 class ProductRow extends Component {
+  constructor() {
+    super();
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+
+  async handleAdd(evt) {
+    evt.preventDefault();
+    await this.props.addToCart(this.props.product.id);
+  }
+
+  async handleRemove(evt) {
+    evt.preventDefault();
+    await this.props.removeFromCart(this.props.product.id);
+  }
+
   render() {
+    const product = this.props.product;
     return (
-      <div className='container-fluid text-center'>
-        <div className='row text-center'>
-          <div className='col-4'> Product Image </div>
-          <div className='col-2'> Product Name </div>
-          <div className='col-2'> Price </div>
-          <div className='col-2'>
-            Quantity
-            <button>+</button>
-            <button>-</button>
+      <div className="container-fluid text-center">
+        <div className="row text-center">
+          <div className="col-4"> Product Image </div>
+          <div className="col-2"> {product.name} </div>
+          <div className="col-2"> {product.price} </div>
+          <div className="col-2">
+            {product.quantity}
+            <button type="button" onClick={this.handleAdd}>
+              +
+            </button>
+            <button type="button" onClick={this.handleRemove}>
+              -
+            </button>
           </div>
         </div>
       </div>
@@ -29,12 +51,11 @@ class ProductRow extends Component {
  * CONTAINER
  */
 
-const mapStateToProps = state => ({
-
-});
+const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({
-
+  addToCart: id => dispatch(addToCart(id)),
+  removeFromCart: id => dispatch(removeFromCart(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductRow);
