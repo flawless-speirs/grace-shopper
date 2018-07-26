6c114e33e5c453a23154f1afc0abb22b8c60d82a
fdterr <fdterr@gmail.com>
2018-07-26 11:54:52 -0400

    Update SingleProduct.js
    cleanup

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -44,28 +44,3 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
-
-// import React from 'react'
-// import { addToCart } from '../store/user'
-// import { connect } from 'react-redux';
-
-// export const SingleProduct = props => {
-//   const product = props.product
-//   return (
-//     <div>
-//       <div className="product-image"><img src={product.imageUrl} /></div>
-//       <div className="product-name">{product.name}</div>
-//       <div className="product-price">{product.price}</div>
-//       <div className="product-description">{product.description}</div>
-//       <button type="submit" onClick={() => props.addToCart(props.product)}>Add To Cart</button>
-//     </div>
-//   )
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     addToCart: (product) => dispatch(addToCart(product))
-//   }
-// }
-
-// export default connect(null, mapDispatchToProps)(SingleProduct)
