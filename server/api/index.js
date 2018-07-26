28db288ce3cc3c6253103e31bcf8df0d69b21cf6
Rick Terry <fdterr@gmail.com>
2018-07-26 14:58:12 -0400

    basic strategy used to protect all api routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,20 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+const passport = require('passport');
+module.exports = router;
 
-router.use('/users', require('./users'))
+router.use(
+  '/',
+  passport.authenticate('basic', { session: false }),
+  (req, res, next) => {
+    next();
+  }
+),
+  router.use('/users', require('./users'));
 
-router.use('/products', require('./products'))
+router.use('/products', require('./products'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
--- server/api/products.js
+++ server/api/products.js
@@ -1,21 +1,22 @@
-const router = require('express').Router()
-const {Product} = require('../db/models')
-module.exports = router
+const router = require('express').Router();
+const { Product } = require('../db/models');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
+  console.log(req.user);
   try {
-    const products = await Product.findAll()
-    res.json(products)
+    const products = await Product.findAll();
+    res.json(products);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const product = await Product.findById(req.params.id)
-    res.json(product)
+    const product = await Product.findById(req.params.id);
+    res.json(product);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
