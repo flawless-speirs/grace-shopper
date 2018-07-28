49c6571a0c34ff33fed488d5db3bae259a7ed0d0
Andrew Miller <admiller9@gmail.com>
2018-07-28 10:29:19 -0400

    added display message in navbar when logged in

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,6 +14,7 @@ const Navbar = ({ handleClick, isLoggedIn, userEmail }) => {
         {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
+            Logged in as {userEmail}
             <Link to="/home" className="btn nav-btn">
               Home
             </Link>
