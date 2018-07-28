2c6edfcebe2d78b5e611ec3d5a17935d88c149fe
Andrew Miller <admiller9@gmail.com>
2018-07-28 09:17:37 -0400

    don't get cart on refresh (cart should persist, soon)

--- client/store/user.js
+++ client/store/user.js
@@ -26,7 +26,6 @@ export const me = () => async dispatch => {
   try {
     const res = await axios.get('/auth/me');
     dispatch(getUser(res.data || defaultUser));
-    dispatch(getCartFromDB());
   } catch (err) {
     console.error(err);
   }
