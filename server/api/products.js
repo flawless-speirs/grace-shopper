bc4692b9fd802a6b5dd4f0e4b341e25e7e655518
Andrew Miller <admiller9@gmail.com>
2018-07-30 14:28:25 -0400

    added tag model, defined associations between product and tag, added tag seed and association seed

--- server/api/products.js
+++ server/api/products.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { Product } = require('../db/models');
+const { Product, Tag } = require('../db/models');
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Product.findAll();
+    const products = await Product.findAll({
+      include: { model: Tag, through: { attributes: [] } },
+    });
     res.json(products);
   } catch (err) {
     next(err);
@@ -13,7 +15,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id, { include: [Tag] });
     res.json(product);
   } catch (err) {
     next(err);
