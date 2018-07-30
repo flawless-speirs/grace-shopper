e8e369a602c720a04106101ebf144c0379e6bf44
Kevin Ho <bornarkun@gmail.com>
2018-07-30 16:16:02 -0400

    Removed react-popup import

--- client/components/AccountPreferences.js
+++ client/components/AccountPreferences.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { changePassword } from '../store/user';
-import Popup from 'reactjs-popup';
 
 class AccountPreferences extends Component {
   constructor({ user, passwordError }) {
