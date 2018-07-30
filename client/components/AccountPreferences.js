9b79c62cfccfcc1bdbc61be1298e7858bd9cd6fd
Kevin Ho <bornarkun@gmail.com>
2018-07-30 16:15:38 -0400

    Removed Modal import

--- client/components/AccountPreferences.js
+++ client/components/AccountPreferences.js
@@ -1,4 +1,4 @@
-import React, { Component, Modal } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { changePassword } from '../store/user';
 import Popup from 'reactjs-popup';
