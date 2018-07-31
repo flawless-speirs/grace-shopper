23104315af557ed763069ad00e7faac545338471
Rick Terry <fdterr@gmail.com>
2018-07-31 18:09:23 -0400

    helmet middleware added

--- server/index.js
+++ server/index.js
@@ -12,8 +12,14 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const socketio = require('socket.io');
 const stripe = require('stripe')('sk_test_TZtWjhogwDxDLuKJZwwhZ3tO');
+const helmet = require('helmet');
 module.exports = app;
 
+/**
+ * Helmet Middleware
+ */
+
+app.use(helmet());
 // This is a global Mocha hook, used for resource cleanup.
 // Otherwise, Mocha v4+ never quits after tests.
 if (process.env.NODE_ENV === 'test') {
